ac50389f7b7f07bfe00ad8c6d5991974f4135158
atikabdullah <atikabdullahjoker@gmail.com>
2021-02-01 12:26:40 +0600

    requirements txt file added

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^ql!_=4r0f6jgt-irh69bdmxn$&-#hjcsk*as&y^n7@z5sbt%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.01','']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from generator import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('',views.home, name='home' ),
     path('password.html',views.password),
     # path('generatedpassword',views.password,name='password'),
+        url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
